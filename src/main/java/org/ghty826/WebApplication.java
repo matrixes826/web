ed8d371ce623e85185a93e25f6add688959722f3
matrixes@163.com <matrixes826>
2017-01-07 16:43:28 +0800

    new web

---
+++ src/main/java/org/ghty826/WebApplication.java
@@ -0,0 +1,12 @@
+package org.ghty826;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+}
