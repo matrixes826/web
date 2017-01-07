ce7f4024b12d5eae58750f497925f0d65957db13
matrixes@163.com <matrixes826>
2017-01-08 03:32:01 +0800

    add web

--- src/main/java/org/ghty826/WebApplication.java
+++ src/main/java/org/ghty826/WebApplication.java
@@ -1,12 +1,27 @@
 package org.ghty826;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+	private static final Logger logger = LoggerFactory.getLogger(WebApplication.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		logger.debug("configure start");
+		return application.sources(WebApplication.class);
+	}
 
 	public static void main(String[] args) {
+		logger.debug("main start");
 		SpringApplication.run(WebApplication.class, args);
+		logger.debug("main start success");
 	}
+
 }
---
+++ src/main/java/org/ghty826/web/GreetingController.java
@@ -0,0 +1,14 @@
+package org.ghty826.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreetingController {
+
+	@GetMapping("greeting")
+	public String greeting() {
+		return "home";
+	}
+
+}
